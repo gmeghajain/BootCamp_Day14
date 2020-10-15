999cc7b11f5e5bac2ce6b884a755f907023ac4e5
gmeghajain <gmeghajain@gmail.com>
2020-10-15 16:57:27 +0530

    valid last name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
 # user registration problem
+#As a user need to enter valid lst name 
+#starts with capital latter and has minimum 3 characters
+
+read -p "enter last name:" lastName
+
+validLastName="^([A-Z]+)([A-Za-z]{2,15})$"
+
+if [[ $lastName =~ $validLastName ]]
+then
+	echo "Entered valid last name!"
+else
+	echo "Entered invalid last name!"
+fi
