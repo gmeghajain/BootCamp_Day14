d145cf116046c87ac5d383e022df2d50e0aae763
gmeghajain <gmeghajain@gmail.com>
2020-10-15 17:21:18 +0530

    valid email

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,1 +1,17 @@
+#! /bin/bash -x
+
+# user registration problem
+#As a user need to enter valid email
+#abc@bl.co.in all parts mandetory
+
+read -p "enter email id" email
+
+validEmail="^([a-zA-Z\.]+)([A-Za-z]+)@([a-z]+)\.([a-zA-Z]\.]{2,5})$"
+
+if [[ $Email =~ $validEmail ]]
+then
+        echo "Entered valid email!"
+else
+        echo "Entered invalid email!"
+fi
 
